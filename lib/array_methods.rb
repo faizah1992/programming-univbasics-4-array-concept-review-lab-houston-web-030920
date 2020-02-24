ba8013ab6792827f9569cab123b076e2c9a70fbb
FAIZAH AHSAN <faizah1992@gmail.com>
2020-02-24 15:25:44 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,12 +1,10 @@
 def find_element_index(array, value_to_find)
- length = array.length
- length.times do |index|
-  puts array[index]
-end
+
+
 end
 
 def find_max_value(array)
-  array.find_max_value
+  array.max()
 end
 
 def find_min_value(array)
